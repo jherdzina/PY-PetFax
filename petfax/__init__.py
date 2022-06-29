5004cc032593e95cdc5784187da86bfffdafacd5
Jennifer Herdzina <92278996+jherdzina@users.noreply.github.com>
2022-06-29 18:38:24 -0500

    creating pet blueprint view

--- petfax/__init__.py
+++ petfax/__init__.py
@@ -1,10 +1,28 @@
 from flask import Flask 
 
-def create_app(): 
+# def create_app(): 
+#     app = Flask(__name__)
+
+#     @app.route('/')
+#     def hello(): 
+#         return 'Hello, PetFax!'
+
+#     return app
+
+
+
+
+# factory
+def create_app():
     app = Flask(__name__)
 
     @app.route('/')
     def hello(): 
         return 'Hello, PetFax!'
 
+    # register pet blueprint 
+    from . import pet
+    app.register_blueprint(pet.bp)
+
+    # return the app 
     return app
